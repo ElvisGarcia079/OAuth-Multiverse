940247605f0df5754931f72aadde5348c978e684
Elvis Garcia <elvisgarcia93@gmail.com>
2023-07-19 14:08:25 -0400

    Update server.js
    Include oidc on line 32 as part of the entire object inherited by OAuth

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
+// req.oidc.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
